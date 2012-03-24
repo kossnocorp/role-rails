6874dbf72a3020140d1208056bb63b1c6b85ee28
Igor Alexandrov <igor.alexandrov@gmail.com>
2012-03-24 22:24:32 +0400

    Updated Slim initialization

--- lib/role-rails/engine.rb
+++ lib/role-rails/engine.rb
@@ -5,7 +5,8 @@ module Role
     class Engine < ::Rails::Engine
       initializer "role-rails.register" do |app|
         if defined?(Slim::Engine)
-          Slim::Engine.default_options[:shortcut] = {'#' => 'div id', '.' => 'div class', '@' => 'div role'}
+          Slim::Engine.default_options[:shortcut] ||= {}
+          Slim::Engine.default_options[:shortcut].merge!('@' => 'div role')
         end
       end      
     end
