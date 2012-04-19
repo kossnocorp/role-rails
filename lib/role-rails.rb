8aff99cc58ebb40ddef69984df3339f2620d9c1f
Pavel Pravosud <rwz@duckroll.ru>
2012-04-20 00:41:45 +0700

    Removed some explict requires

--- lib/role-rails.rb
+++ lib/role-rails.rb
@@ -1,6 +1,2 @@
-require 'rails'
 require 'role-rails/version'
-
-module RoleRails
-  require 'role-rails/engine'
-end
+require 'role-rails/engine'
--- lib/role-rails/engine.rb
+++ lib/role-rails/engine.rb
@@ -1,12 +1,7 @@
-require 'rails'
-
 module RoleRails
   class Engine < ::Rails::Engine
     initializer "role-rails.register" do |app|
-      if defined?(Slim::Parser)
-        shortcut = Slim::Parser.default_options[:shortcut] || {}
-        Slim::Parser.default_options[:shortcut] = shortcut.merge('@' => 'div role')
-      end
+      defined?(Slim::Parser) && Slim::Parser.default_options[:shortcut].try(:[]=, '@', 'div role')
     end
   end
 end
