7262a48b94d6dae059dd8514334629a3fcda7be6
Andrey Ognevsky <a.ognevsky@gmail.com>
2013-03-06 18:38:14 +0100

    Slim :shortcut string values are deprecated

--- lib/role-rails/engine.rb
+++ lib/role-rails/engine.rb
@@ -2,7 +2,7 @@ module RoleRails
   class Engine < ::Rails::Engine
     initializer "role-rails.register" do |app|
       if defined?(Slim::Parser)
-        Slim::Parser.default_options[:shortcut].try(:[]=, '@', 'role')
+        Slim::Parser.default_options[:shortcut].try(:[]=, '@', :attr => 'role')
       end
 
       if defined?(Slim::Engine)
