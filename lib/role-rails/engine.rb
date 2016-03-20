1dd9a63345e76e7615ce57ea5fa313a02192e3e0
Alex Timofeev <atimofeev@reactant.ru>
2016-03-20 10:27:38 +0700

    Use `options` method rather than `default_options`
    It makes gem compatible with newer versions of Slim (>= 3.0.0)

--- lib/role-rails/engine.rb
+++ lib/role-rails/engine.rb
@@ -2,12 +2,16 @@ module RoleRails
   class Engine < ::Rails::Engine
     initializer "role-rails.register" do |app|
       if defined?(Slim::Parser)
-        Slim::Parser.default_options[:shortcut].try(:[]=, '@', :attr => 'role')
+        Slim::Parser.send(slim_options)[:shortcut].try(:[]=, '@', :attr => 'role')
       end
 
       if defined?(Slim::Engine)
-        Slim::Engine.default_options[:merge_attrs].try(:[]=, 'role', ' ')
+        Slim::Engine.send(slim_options)[:merge_attrs].try(:[]=, 'role', ' ')
       end
+    end
+
+    def slim_options
+      Slim::VERSION.to_i >= 3 ? "options" : "default_options"
     end
   end
 end
